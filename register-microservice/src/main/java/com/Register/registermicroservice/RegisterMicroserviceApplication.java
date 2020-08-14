f7aaf8b21bf3eca838f4a3039f7e5b16c55f4c4d
D3stros <patrick.fallmann@hotmail.com>
2020-08-14 15:33:39 +0200

    Sending activation email asynchronously

--- register-microservice/src/main/java/com/Register/registermicroservice/RegisterMicroserviceApplication.java
+++ register-microservice/src/main/java/com/Register/registermicroservice/RegisterMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.Register.registermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class RegisterMicroserviceApplication {
 
 	public static void main(String[] args) {
