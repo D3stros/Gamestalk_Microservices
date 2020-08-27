b2a4bf23a1a15d5a6a4156686c30d2152d995d8c
D3stros <patrick.fallmann@hotmail.com>
2020-08-27 22:01:49 +0200

    Created Zuul API gateway

--- login-microservice/src/main/java/com/loginmicroservices/loginmicroservices/LoginMicroservicesApplication.java
+++ login-microservice/src/main/java/com/loginmicroservices/loginmicroservices/LoginMicroservicesApplication.java
@@ -2,8 +2,10 @@ package com.loginmicroservices.loginmicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LoginMicroservicesApplication {
 
 	public static void main(String[] args) {
